a86905a46f20c121983dd1b99ab76cd3640ec31e
oswalgopal <gploswal@gmail.com>
2021-05-06 07:34:25 +0530

    added api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var editor = ace.edit("editor");
+var beautify = ace.require("ace/ext/beautify");
 editor.setTheme("ace/theme/ambiance");
 editor.session.setMode("ace/mode/c_cpp");
+const selectedLanguage = document.getElementById("selectedLanguage");
+selectedLanguage.innerText = "C (GCC 9.1.0)";
 let selectedLangObj = {
     label: "C (GCC 9.1.0)",
     lang: "c",
@@ -10,19 +13,34 @@ let selectedLangObj = {
 function run() {
     var code = editor.getValue();
     console.log(code);
+    let headers = new Headers();
+
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    headers.append('Origin','http://localhost:3000');
     fetch("https://vv-ide.herokuapp.com/compile", {
         method: "POST",
-        body: {
+        headers,
+        mode: 'cors',
+        body: JSON.stringify({
             "script" : code,
             "language": selectedLangObj.lang,
             "versionIndex": selectedLangObj.version,
             "stdin": document.getElementById('stdin').value
-        }
+        })
     }).then(res => {
         console.log(res);
+        res.json().then((response) => {
+            console.log(response);
+            document.getElementById("compile_output").innerText = response.response.output;
+        }).catch(error => {
+            console.log(error);
+            alert("Could not compile code try later");
+        })
     }).catch(error => {
         console.log(error);
-    })
+        alert("Could not compile code try later");
+    });
 }
 
 function showSettings() {
@@ -57,6 +75,8 @@ function saveSettings() {
     editor.setTheme("ace/theme/" + document.getElementById("themeSelect").value);
     const lang = JSON.parse(document.getElementById("languageSelect").value);
     editor.session.setMode("ace/mode/" + lang.mode);
+    selectedLangObj = lang;
+    selectedLanguage.innerText = lang.label;
     console.log(lang);
 
     $('#settings').hide();
@@ -66,3 +86,6 @@ function cancelSettings(){
     $('#settings').hide();
 }
 
+function beautifyCode() {
+    beautify.beautify(editor.session);
+}
