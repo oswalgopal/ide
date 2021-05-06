d05d31f4a7f27d81dcda0e82a4540343fb2eb438
oswalgopal <gploswal@gmail.com>
2021-05-06 11:23:09 +0530

    added language

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var editor = ace.edit("editor");
 var beautify = ace.require("ace/ext/beautify");
+console.log(beautify)
 editor.setTheme("ace/theme/ambiance");
 editor.session.setMode("ace/mode/c_cpp");
 const selectedLanguage = document.getElementById("selectedLanguage");
+document.getElementById('editor').style.fontSize = "14px";
 selectedLanguage.innerText = "C (GCC 9.1.0)";
+// editor.session.setValue("// start your coding here");
+
 let selectedLangObj = {
     label: "C (GCC 9.1.0)",
     lang: "c",
@@ -11,8 +15,15 @@ let selectedLangObj = {
     mode: "c_cpp"
 };
 function run() {
+    const run_btn = document.getElementById("run_btn");
+    run_btn.innerText = "Compiling";
     var code = editor.getValue();
     console.log(code);
+    if (!code) {
+        alert("Please Enter write code ...");
+        run_btn.innerText = "Run";
+        return;
+    }
     let headers = new Headers();
 
     headers.append('Content-Type', 'application/json');
@@ -33,19 +44,25 @@ function run() {
         res.json().then((response) => {
             console.log(response);
             document.getElementById("compile_output").innerText = response.response.output;
+            document.getElementById("compile_time").innerText = "Time: " +response.response.cpuTime + " ms";
+            document.getElementById("compile_memory").innerText = "Memory: " + response.response.memory + " bytes";
+            run_btn.innerText = "Run";
         }).catch(error => {
             console.log(error);
             alert("Could not compile code try later");
+            run_btn.innerText = "Run";
         })
     }).catch(error => {
         console.log(error);
         alert("Could not compile code try later");
+        run_btn.innerText = "Run";
     });
 }
 
 function showSettings() {
     // $('#settings').show();
     $('#settings').css("display", "flex");
+    // $('#settings').addClass("animation_class");
 }
 
 $('#settings').click(function(ev) {
@@ -65,9 +82,10 @@ function saveSettings() {
     }
     // console.log(Boolean(activeLineSelectValue));
     // editor.setHighlightActiveLine(activeLineSelectValue);
-    editor.setOptions({
-        setHighlightActiveLine: activeLineSelectValue
-    });
+    // editor.setOptions({
+    //     setHighlightActiveLine: activeLineSelectValue
+    // });
+    editor.setShowPrintMargin(activeLineSelectValue);
 
     // console.log(document.getElementById("tabSizeSelect").value);
     editor.session.setTabSize(Number(document.getElementById("tabSizeSelect").value));
@@ -79,6 +97,8 @@ function saveSettings() {
     selectedLanguage.innerText = lang.label;
     console.log(lang);
 
+    document.getElementById('editor').style.fontSize = document.getElementById("fontSizeSelect").value + "px";
+
     $('#settings').hide();
 }
 
@@ -88,4 +108,10 @@ function cancelSettings(){
 
 function beautifyCode() {
     beautify.beautify(editor.session);
+}
+
+function clearCode() {
+    let confirm = window.confirm("Are you sure you want to reset code \nYou cannot get this code back");
+    if (confirm)
+        editor.session.setValue("// write your code here");
 }
--- showLanguages.js
+++ showLanguages.js
@@ -1,15 +1,140 @@
 const languages = [
     {
-        label: "C (GCC 9.1.0)",
+        label: "C (GCC 5.3.0)",
         lang: "c",
         version: "0",
         mode: "c_cpp"
     },
     {
-        label: "C++ (G++ 14 GCC 9.1.0)",
+        label: "C (Zapcc 5.0.0)",
+        lang: "c",
+        version: "1",
+        mode: "c_cpp"
+    },
+    {
+        label: "C (GCC 7.2.0)",
+        lang: "c",
+        version: "2",
+        mode: "c_cpp"
+    },
+    {
+        label: "C (GCC 8.1.0)",
+        lang: "c",
+        version: "3",
+        mode: "c_cpp"
+    },
+    {
+        label: "C (GCC 9.1.0)",
+        lang: "c",
+        version: "4",
+        mode: "c_cpp"
+    },
+    // done
+    {
+        label: "C++ (GCC 5.3.0)",
+        lang: "cpp",
+        version: "0",
+        mode: "c_cpp"
+    },
+    {
+        label: "C++ (Zapcc 5.0.0)",
+        lang: "cpp",
+        version: "1",
+        mode: "c_cpp"
+    },
+    {
+        label: "C++ (GCC 7.2.0)",
+        lang: "cpp",
+        version: "2",
+        mode: "c_cpp"
+    },
+    {
+        label: "C++ (GCC 8.1.0)",
+        lang: "cpp",
+        version: "3",
+        mode: "c_cpp"
+    },
+    {
+        label: "C++ (GCC 9.1.0)",
+        lang: "cpp",
+        version: "4",
+        mode: "c_cpp"
+    },
+    // done
+    {
+        label: "C++ 14 (g++ 14 GCC 5.3.0)",
         lang: "cpp14",
         version: "0",
         mode: "c_cpp"
+    },
+    {
+        label: "C++ 14 (g++ 14 GCC 7.2.0)",
+        lang: "cpp14",
+        version: "2",
+        mode: "c_cpp"
+    },
+    {
+        label: "C++ 14 (g++ 14 GCC 8.1.0)",
+        lang: "cpp14",
+        version: "3",
+        mode: "c_cpp"
+    },
+    {
+        label: "C++ 14 (g++ 14 GCC 9.1.0)",
+        lang: "cpp14",
+        version: "4",
+        mode: "c_cpp"
+    },
+    // Done
+    {
+        label: "C++ 17 (g++ 14 GCC 9.10)",
+        lang: "cpp17",
+        version: "1",
+        mode: "c_cpp"
+    },
+    // done
+    {
+        label: "C99 (GCC 5.3.0)",
+        lang: "c99",
+        version: "0",
+        mode: "c_cpp"
+    },
+    {
+        label: "C99 (GCC 7.2.0)",
+        lang: "c99",
+        version: "2",
+        mode: "c_cpp"
+    },
+    {
+        label: "C99 (GCC 8.1.0)",
+        lang: "c99",
+        version: "3",
+        mode: "c_cpp"
+    },
+    {
+        label: "C99 (GCC 9.1.0)",
+        lang: "c99",
+        version: "4",
+        mode: "c_cpp"
+    },
+    // Done
+    {
+        label: "Java (JDK 1.8.0_66)",
+        lang: "java",
+        version: "0",
+        mode: 'java'
+    },
+    {
+        label: "Java (9.0.1)",
+        lang: "java",
+        version: "1",
+        mode: 'java'
+    },
+    {
+        label: "Java (10.0.1)",
+        lang: "java",
+        version: "2",
+        mode: 'java'
     },
     {
         label: "Java (11.0.4)",
@@ -17,11 +142,249 @@ const languages = [
         version: "3",
         mode: 'java'
     },
+    // Done
+    {
+        label: "PHP (5.6.16)",
+        lang: "php",
+        version: "0",
+        mode: "php"
+    },
+    {
+        label: "PHP (7.1.11)",
+        lang: "php",
+        version: "1",
+        mode: "php"
+    },
+    {
+        label: "PHP (7.2.5)",
+        lang: "php",
+        version: "2",
+        mode: "php"
+    },
+    {
+        label: "PHP (7.3.10)",
+        lang: "php",
+        version: "3",
+        mode: "php"
+    },
+    // Done
+    {
+        label: "Perl (5.22.0)",
+        lang: "perl",
+        version: "0",
+        mode: "perl"
+    },
+    {
+        label: "Perl (5.26.1)",
+        lang: "perl",
+        version: "1",
+        mode: "perl"
+    },
+    {
+        label: "Perl (5.26.2)",
+        lang: "perl",
+        version: "2",
+        mode: "perl"
+    },
+    {
+        label: "Perl (5.30.0)",
+        lang: "perl",
+        version: "3",
+        mode: "perl"
+    },
+    // Done
+    {
+        label: "Python 2 (2.7.11)",
+        lang: "python2",
+        version: "0",
+        mode: "python"
+    },
+    {
+        label: "Python 2 (2.7.15)",
+        lang: "python2",
+        version: "1",
+        mode: "python"
+    },
+    {
+        label: "Python 2 (2.7.16)",
+        lang: "python2",
+        version: "2",
+        mode: "python"
+    },
+    // Done
+    {
+        label: "Python 3 (3.5.1)",
+        lang: "python3",
+        version: "0",
+        mode: "python"
+    },
+    {
+        label: "Python 3 (3.6.3)",
+        lang: "python3",
+        version: "1",
+        mode: "python"
+    },
+    {
+        label: "Python 3 (3.6.5)",
+        lang: "python3",
+        version: "2",
+        mode: "python"
+    },
     {
         label: "Python 3 (3.7.4)",
         lang: "python3",
         version: "3",
         mode: "python"
+    },
+    // Done
+    {
+        label: "Bash Shell (4.3.42)",
+        lang: "bash",
+        version: "0",
+        mode: "bash"
+    },
+    {
+        label: "Bash Shell (4.4.12)",
+        lang: "bash",
+        version: "1",
+        mode: "bash"
+    },
+    {
+        label: "Bash Shell (4.4.19)",
+        lang: "bash",
+        version: "2",
+        mode: "bash"
+    },
+    {
+        label: "Bash Shell (5.0.011)",
+        lang: "bash",
+        version: "3",
+        mode: "bash"
+    },
+    // Done
+    {
+        label: "Ruby (2.2.4)",
+        lang: "ruby",
+        version: "0",
+        mode: "ruby"
+    },
+    {
+        label: "Ruby (2.4.2p198)",
+        lang: "ruby",
+        version: "1",
+        mode: "ruby"
+    },
+    {
+        label: "Ruby (2.5.1p57)",
+        lang: "ruby",
+        version: "2",
+        mode: "ruby"
+    },
+    {
+        label: "Ruby (2.6.5)",
+        lang: "ruby",
+        version: "3",
+        mode: "ruby"
+    },
+    // Done
+    {
+        label: "GO Lang (1.5.2)",
+        lang: "go",
+        version: "0",
+        mode: "golang"
+    },
+    {
+        label: "GO Lang (1.9.2)",
+        lang: "go",
+        version: "1",
+        mode: "golang"
+    },
+    {
+        label: "GO Lang (1.10.2)",
+        lang: "go",
+        version: "2",
+        mode: "golang"
+    },
+    {
+        label: "GO Lang (1.10.1)",
+        lang: "go",
+        version: "3",
+        mode: "golang"
+    },
+    // Done
+    {
+        label: "Scala (2.12.0)",
+        lang: "scala",
+        version: "0",
+        mode: "scala"
+    },
+    {
+        label: "Scala (2.12.4)",
+        lang: "scala",
+        version: "1",
+        mode: "scala"
+    },
+    {
+        label: "Scala (2.12.5)",
+        lang: "scala",
+        version: "2",
+        mode: "scala"
+    },
+    {
+        label: "Scala (2.13.0)",
+        lang: "scala",
+        version: "3",
+        mode: "scala"
+    },
+    // Done
+    {
+        label: "SQL (SQLite 3.9.2)",
+        lang: "sql",
+        version: "0",
+        mode: "sql"
+    },
+    {
+        label: "SQL (SQLite 3.21.0)",
+        lang: "sql",
+        version: "1",
+        mode: "sql"
+    },
+    {
+        label: "SQL (SQLite 3.23.0)",
+        lang: "sql",
+        version: "2",
+        mode: "sql"
+    },
+    {
+        label: "SQL (SQLite 3.29.0)",
+        lang: "sql",
+        version: "3",
+        mode: "sql"
+    },
+    // Done
+    {
+        label: "NodeJs (6.3.1)",
+        lang: "nodejs",
+        version: "0",
+        mode: "javascript"
+    },
+    {
+        label: "NodeJs (9.2.0)",
+        lang: "nodejs",
+        version: "1",
+        mode: "javascript"
+    },
+    {
+        label: "NodeJs (10.1.0)",
+        lang: "nodejs",
+        version: "2",
+        mode: "javascript"
+    },
+    {
+        label: "NodeJs (12.11.1)",
+        lang: "nodejs",
+        version: "3",
+        mode: "javascript"
     },
 ]
 
